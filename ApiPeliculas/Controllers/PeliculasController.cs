7c79537d154702be6d65119d5b518161a9bdb301
Marco Cruz <marco24cruz08@gmail.com>
2024-07-21 15:39:09 -0600

    chore: Add PeliculasController and PeliculaRepositorio to API

---
+++ ApiPeliculas/Controllers/PeliculasController.cs
@@ -0,0 +1,51 @@
+﻿using ApiPeliculas.Modelos.DTOs;
+using ApiPeliculas.Repositorio;
+using ApiPeliculas.Repositorio.IRepositorio;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiPeliculas.Controllers
+{
+    [Route("api/peliculas")]
+    [ApiController]
+    public class PeliculasController : ControllerBase
+    {
+        private readonly IPeliculaRepositorio _peliculaRepositorio;
+        private readonly IMapper _mapper;
+
+        public PeliculasController(IPeliculaRepositorio peliculaRepositorio, IMapper mapper)
+        {
+            _peliculaRepositorio = peliculaRepositorio;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult GetCategorias()
+        {
+            var listaPeliculas = _peliculaRepositorio.GetPeliculas();
+            var listaPeliculasDto = new List<CategoriaDTO>();
+            foreach (var lista in listaPeliculas)
+            {
+                listaPeliculasDto.Add(_mapper.Map<CategoriaDTO>(lista));
+            }
+            return Ok(listaPeliculasDto);
+        }
+
+        [HttpGet("{peliculaId:int}", Name = "GetPelicula")]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult GetPelicula(int peliculaId)
+        {
+            var itemPelicula = _peliculaRepositorio.GetPelicula(peliculaId);
+            if (itemPelicula == null) return NotFound();
+            var itemPeliculaDTO = _mapper.Map<PeliculaDTO>(itemPelicula);
+
+            return Ok(itemPeliculaDTO);
+        }
+    }
+}
