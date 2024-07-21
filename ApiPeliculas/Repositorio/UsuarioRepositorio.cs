6dee595bf6f41266019145e32a3d0b616ac56fce
Marco Cruz <marco24cruz08@gmail.com>
2024-07-21 16:43:12 -0600

    chore: Add UsuarioRepositorio to handle user-related data

---
+++ ApiPeliculas/Repositorio/UsuarioRepositorio.cs
@@ -0,0 +1,60 @@
+﻿using ApiPeliculas.Data;
+using ApiPeliculas.Modelos;
+using ApiPeliculas.Modelos.DTOs;
+using ApiPeliculas.Repositorio.IRepositorio;
+using XSystem.Security.Cryptography;
+
+namespace ApiPeliculas.Repositorio
+{
+    public class UsuarioRepositorio: IUsuarioRepositorio
+    {
+        private readonly ApplicationDbContext _bd;
+        public UsuarioRepositorio(ApplicationDbContext bd)
+        {
+            _bd = bd;
+        }
+
+        public Usuario GetUsuario(int usuarioId)
+        {
+            return _bd.Usuario.FirstOrDefault(u => u.Id == usuarioId);
+        }
+
+        public ICollection<Usuario> GetUsuarios()
+        {
+            return _bd.Usuario.OrderBy(u => u.NombreUsuario).ToList();
+        }
+
+        public bool IsUniqueUser(string usuario)
+        {
+            var usuarioExiste = _bd.Usuario.Any(u => u.NombreUsuario.ToLower().Trim() == usuario.ToLower().Trim());
+            return usuarioExiste;
+        }
+
+        public async Task<Usuario> Registro(UsuarioRegistroDTO usuarioRegistroDTO)
+        {
+            var passwordEncriptado = ObtenerMD5(usuarioRegistroDTO.Password);
+            Usuario usuario = new Usuario
+            {
+                NombreUsuario = usuarioRegistroDTO.NombreUsuario,
+                Nombre = usuarioRegistroDTO.Nombre,
+                Password = passwordEncriptado,
+                Role = usuarioRegistroDTO.Role
+            };
+            _bd.Usuario.Add(usuario);
+            await _bd.SaveChangesAsync();
+            usuario.Password = "";
+            return usuario;
+        }
+
+        public static string ObtenerMD5(string valor)
+        {
+            MD5CryptoServiceProvider x = new();
+            byte[] data = System.Text.Encoding.UTF8.GetBytes(valor);
+            data = x.ComputeHash(data);
+            string resp = "";
+            for (int i = 0; i < data.Length; i++)
+                resp += data[i].ToString("x2").ToLower();
+            return resp;
+        }
+    }
+}
